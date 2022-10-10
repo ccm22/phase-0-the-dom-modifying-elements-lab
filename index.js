8b35e06c6d45a27193c741f4d34c7357d015c670
Chris Masters <ccm22_@outlook.com>
2022-10-10 20:53:13 +1100

    commit tree e769b065915d1384423933368f6a697e0acdac5f

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+// Make sure you remove the <main> with id 'main': expected <main id="main"></main> to not exist.
+
+main.remove();
+
+// has a 'newHeader' variable that points to an <h1> node.
+
+const newHeader = document.createElement("h1");
+
+// The 'newHeader' variable that points to the <h1> node has an id of 'victory'.
+
+newHeader.id = 'victory';
+
+// The 'newHeader' variable that points to the <h1> node with an id of 'victory' has the text "YOUR-NAME is the champion" (with your name in place of YOUR-NAME) inside it.
+
+newHeader.innerHTML = "Chris is the champion"
